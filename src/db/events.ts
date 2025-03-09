8e9c1cd026223d71d86820a70d1ec56e8a8c4b4a
Sahith Nallapareddy <sahith.reddy@gmail.com>
2025-03-08 23:42:28 -0500

    update comment

--- src/db/events.ts
+++ src/db/events.ts
@@ -9,5 +9,10 @@ export type MaddenBroadcastEvent = { title: string, video: string }
 export type YoutubeBroadcastEvent = { video: string }
 export type AddChannelEvent = { channel_id: string, discord_server: string }
 export type RemoveChannelEvent = { channel_id: string, discord_server: string }
+<<<<<<< Updated upstream
 export type ConfirmedSim = { confirmedUsers: UserId[], requestedUsers: UserId[], result: SimResult, scheduleId: number, seasonIndex: number, week: number, homeUser?: UserId, awayUser?: UserId }
+=======
+// [March 8 2025] league id was added later once we found out that schedule ids can be shared between leagues
+export type ConfirmedSim = { confirmedUsers: UserId[], requestedUsers: UserId[], result: SimResult, scheduleId: number, seasonIndex: number, week: number, homeUser?: UserId, awayUser?: UserId, leagueId?: string }
+>>>>>>> Stashed changes
 export type DiscordLeagueConnectionEvent = { guildId: string, leagueId: string }
