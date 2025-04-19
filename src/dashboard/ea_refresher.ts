ebc053a99fdc90a4aa34529146b7c5cd4ed66e2d
Sahith Nallapareddy <sahith.reddy@gmail.com>
2025-04-19 18:41:13 -0400

    wip

--- src/dashboard/ea_refresher.ts
+++ src/dashboard/ea_refresher.ts
@@ -10,9 +10,13 @@ interface LatestLeagues {
   getLatestLeagues(): string[]
 }
 
-function getLatestLeagues(): LatestLeagues {
-  let leagues: string[] = []
-  db.collection("league_data").onSnapshot(querySnapshot => {
+async function getLatestLeagues(): Promise<LatestLeagues> {
+
+  const collection = db.collection("league_data")
+  const docs = await collection.get()
+  let leagues = docs.docs.map(d => d.id)
+  collection.onSnapshot(querySnapshot => {
+    console.log("Leagues being updated")
     leagues = querySnapshot.docs.map(d => d.id)
   })
   return {
@@ -23,6 +27,7 @@ function getLatestLeagues(): LatestLeagues {
 }
 
 async function checkLeague(leagueId: string) {
+  console.log(`Checking league ${leagueId}`)
   const client = await storedTokenClient(Number(leagueId))
   const leagueData = await client.getLeagueInfo(Number(leagueId))
   const leagueHash = {
@@ -55,14 +60,18 @@ function sleep(ms: number) {
 }
 
 async function runLeagueChecks() {
-  const latestLeagues = getLatestLeagues()
+  const latestLeagues = await getLatestLeagues()
 
   while (true) {
     const leagues = latestLeagues.getLatestLeagues()
     for (const leagueId of leagues) {
       // avoid any overloading of EA
       await sleep(2000)
-      await checkLeague(leagueId)
+      try {
+        await checkLeague(leagueId)
+      } catch (e) {
+        console.error(`Error checking league ${leagueId}: ${e}`)
+      }
     }
     console.log("Check complete, sleeping for 5 minutes...\n")
     await sleep(5 * 60 * 1000)
