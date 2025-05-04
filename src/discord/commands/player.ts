1ad8dcdc8ab2d7d06669d362c29d374cdecaaf8f
Sahith Nallapareddy <sahith.reddy@gmail.com>
2025-05-04 19:11:14 -0400

    w

--- src/discord/commands/player.ts
+++ src/discord/commands/player.ts
@@ -948,7 +948,7 @@ function formatScore(game: MaddenGame) {
 
 enum SnallabotGameResult {
   WIN = "<:snallabot_win:1368708001548079304>",
-  LOSS = "<snallabot_loss:1368708003259482233>",
+  LOSS = "<:snallabot_loss:1368726069963653171>",
   TIE = "<:snallabot_tie:1368713402016337950>"
 }
 
