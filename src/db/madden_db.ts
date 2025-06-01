640ab011fc41b73a9539883ef1b2907ca432ee4f
Sahith Nallapareddy <sahith.reddy@gmail.com>
2025-06-01 13:27:19 -0400

    logging

--- src/db/madden_db.ts
+++ src/db/madden_db.ts
@@ -142,7 +142,6 @@ async function getStats<T extends { rosterId: number }>(leagueId: string, roster
         const histories = await db.collection("league_data").doc(leagueId).collection(collection).doc(docId).collection("history").get()
         const changes = histories.docs.map(d => convertDate(d.data() as StoredHistory))
         const historyStats = reconstructFromHistory<T>(changes, data)
-        console.log(historyStats.filter(d => d.rosterId === rosterId))
         historyStats.push(data)
         const newh = historyStats.filter(d => d.rosterId === rosterId)
         return newh
