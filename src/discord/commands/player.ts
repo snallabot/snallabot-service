0c07d0971157d3abfe51b8f1d9f104dc9903e125
Sahith Nallapareddy <sahith.reddy@gmail.com>
2025-07-13 18:56:01 -0400

    wip

--- src/discord/commands/player.ts
+++ src/discord/commands/player.ts
@@ -95,12 +95,37 @@ async function showPlayerCard(playerSearch: string, client: DiscordClient, token
         custom_id: `${JSON.stringify(pagination)}`
       }
     ] : []
+    console.log([
+      {
+        type: ComponentType.TextDisplay,
+        content: "test"
+        // content: formatPlayerCard(player, teamsDisplayNames)
+      },
+      {
+        type: ComponentType.Separator,
+        divider: true,
+        spacing: SeparatorSpacingSize.Large
+      },
+      {
+        type: ComponentType.ActionRow,
+        components: [
+          {
+            type: ComponentType.StringSelect,
+            custom_id: "player_card",
+            placeholder: formatPlaceholder(PlayerSelection.PLAYER_OVERVIEW),
+            options: generatePlayerOptions(searchRosterId, pagination)
+          }
+        ]
+      },
+      ...backToSearch
+    ])
     await client.editOriginalInteraction(token, {
       flags: 32768,
       components: [
         {
           type: ComponentType.TextDisplay,
-          content: formatPlayerCard(player, teamsDisplayNames)
+          content: "test"
+          // content: formatPlayerCard(player, teamsDisplayNames)
         },
         {
           type: ComponentType.Separator,
