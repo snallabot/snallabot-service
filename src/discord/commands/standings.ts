1224b248d38c29f0f28144ad74b55b4d1185a713
Sahith Nallapareddy <sahith.reddy@gmail.com>
2025-08-29 21:01:08 -0400

    wip

--- src/discord/commands/standings.ts
+++ src/discord/commands/standings.ts
@@ -32,8 +32,7 @@ function getStandingsForFilter(standings: Standing[], filter: string): Standing[
     case "nfc":
       return sortedStandings.filter(s => s.conferenceName.toLowerCase() === "nfc");
     case "afc_north":
-      console.log(sortedStandings.filter(s => s.conferenceName))
-      console.log(sortedStandings.filter(s => s.divisonName))
+      console.log(sortedStandings.map(s => s.divisonName))
       return sortedStandings.filter(s =>
         s.conferenceName.toLowerCase() === "afc" && s.divisonName.toLowerCase().includes("north"));
     case "afc_south":
