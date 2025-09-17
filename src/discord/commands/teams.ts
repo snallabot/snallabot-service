9a5b89a5a9b758a3202d67e6c9fd8b83c5cbe4ed
Sahith Nallapareddy <sahith.reddy@gmail.com>
2025-09-17 00:13:28 -0400

    wip

--- src/discord/commands/teams.ts
+++ src/discord/commands/teams.ts
@@ -233,9 +233,10 @@ export default {
 
       }
     } else if (subCommand === "customize_logo") {
-      if (!teamsCommand.options || !teamsCommand.options[0] || !teamsCommand.options[1]) {
+      if (!teamsCommand.options || !teamsCommand.options[0] || !teamsCommand.resolved) {
         throw new Error("teams customize_logo misconfigured")
       }
+      console.log(teamsCommand)
       const teamSearchPhrase = (teamsCommand.options[0] as APIApplicationCommandInteractionDataStringOption).value.toLowerCase()
       const image = (teamsCommand.options[1] as APIApplicationCommandInteractionDataAttachmentOption)
       if (!leagueSettings?.commands?.madden_league?.league_id) {
