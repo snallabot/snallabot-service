eb6cac2c7b2f8e8ae046df17c711b8a78ba7acf5
Sahith Nallapareddy <sahith.reddy@gmail.com>
2025-09-27 21:17:22 -0400

    wip

--- src/discord/commands/sims.ts
+++ src/discord/commands/sims.ts
@@ -101,7 +101,7 @@ async function showSeasonSims(token: string, client: DiscordClient, league: stri
       label: `Season ${s + MADDEN_SEASON}`,
       value: JSON.stringify({ si: s } as SeasonSelection)
     }))
-
+    console.log(message.length)
     await client.editOriginalInteraction(token, {
       flags: 32768,
       components: [
