0e520554d24e928f2051e126a73b45d3ec0ea37e
Sahith Nallapareddy <sahith.reddy@gmail.com>
2025-10-01 01:51:04 -0400

    wip

--- src/db/madden_db.ts
+++ src/db/madden_db.ts
@@ -409,6 +409,11 @@ const MaddenDB: MaddenDB = {
     EventDB.on(event_type, notifier)
   },
   getLatestTeams: async function(leagueId: string): Promise<TeamList> {
+    let e = new Error();
+    let frame = e?.stack?.split("\n")[2]; // change to 3 for grandparent func
+    let lineNumber = frame?.split(":")?.reverse()[1];
+    let functionName = frame?.split(" ")[5];
+    console.log(functionName + ":" + lineNumber + " ")
     const cachedTeamDocs = teamCache.get(leagueId) as Record<string, StoredEvent<Team>>
     if (cachedTeamDocs) {
       console.log(`used cached data ${cachedTeamDocs}`)
