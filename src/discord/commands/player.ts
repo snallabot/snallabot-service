da58bb6a10ad053d2cd5ebf65ed1fab5a81738f1
Sahith Nallapareddy <sahith.reddy@gmail.com>
2025-12-15 18:00:28 -0500

    wip

--- src/discord/commands/player.ts
+++ src/discord/commands/player.ts
@@ -375,7 +375,7 @@ async function showPlayerList(playerSearch: string, client: DiscordClient, token
     const nextDisabled = players.length < PAGINATION_LIMIT ? true : false
     const nextPagination = players.length === 0 ? startAfterPlayer : players[players.length - 1].rosterId
     const previousPagination = players.length === 0 ? endBeforePlayer : players[0].rosterId
-    console.log({
+    console.dir({
       flags: 32768,
       components: [
         {
