358a37633cba38dfa98bf79e3e01ba652a3667a3
Sahith Nallapareddy <sahith.reddy@gmail.com>
2025-12-19 23:25:45 -0500

    wip

---
+++ src/debug/metrics.ts
@@ -0,0 +1,27 @@
+const client = require('prom-client')
+const register = new client.Registry()
+const collectDefaultMetrics = client.collectDefaultMetrics
+collectDefaultMetrics({ register })
+
+export async function scrapeMetrics() {
+  return await register.metrics()
+}
+
+export const exportCounter = new client.Counter(
+  {
+    name: "madden_exports",
+    help: "number of exports",
+    labelNames: ["export_context", "export_type"],
+    registers: [register]
+  }
+)
+
+export const debugCounter = new client.Counter(
+  {
+    name: "debug_counter",
+    help: "tests metrics in bot",
+    registers: [register]
+  }
+)
+
+export const contentType = register.contentType
