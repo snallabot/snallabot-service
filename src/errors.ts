27005fe5e7e1be6a6a82a39592e74109c6ca74bd
Sahith Nallapareddy <sahith.reddy@gmail.com>
2026-09-16 19:01:02 -0400

    cleaner error message (#212)
    try to space these out so its easier to parse?

--- src/errors.ts
+++ src/errors.ts
@@ -2,7 +2,7 @@ export class SnallabotError extends Error {
   guidance: string
   error: Error
   constructor(error: Error, guidance: string) {
-    super(`An error ocurred! Guidance ${guidance} Original Error: ${error.message}`)
+    super(`An error occurred!\n\nGuidance ${guidance}\n\nOriginal Error: ${error.message}`)
     this.guidance = guidance
     this.error = error
   }
