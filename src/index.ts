cc3f5f20a2b4b7779a246d01944a336c964aa3c3
Sahith Nallapareddy <sahith.reddy@gmail.com>
2026-09-20 01:33:31 -0400

    add latency metrics

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import app from "./server"
+import { latencyMiddleware } from "./debug/metrics"
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
   console.log(`server started on ${port}`);
 });
+
+app.use(latencyMiddleware)
